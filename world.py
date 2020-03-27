8b58ce4f5ed98156dafeed234dd5d736c69b94a9
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-03-26 19:33:34 -0700

    pep 8

--- world.py
+++ world.py
@@ -2,20 +2,30 @@ from room import Room
 import random
 import math
 
+
 class World:
     def __init__(self):
         self.starting_room = None
         self.rooms = {}
         self.room_grid = []
         self.grid_size = 0
+
     def load_graph(self, room_graph):
         num_rooms = len(room_graph)
         rooms = [None] * num_rooms
         grid_size = 1
         for i in range(0, num_rooms):
             x = room_graph[i][0][0]
-            grid_size = max(grid_size, room_graph[i][0][0], room_graph[i][0][1])
-            self.rooms[i] = Room(f"Room {i}", f"({room_graph[i][0][0]},{room_graph[i][0][1]})",i, room_graph[i][0][0], room_graph[i][0][1])
+            grid_size = max(
+                grid_size, room_graph[i][0][0], room_graph[i][0][1]
+                )
+            self.rooms[i] = Room(
+                f'Room {i}',
+                f'({room_graph[i][0][0]},{room_graph[i][0][1]})',
+                i,
+                room_graph[i][0][0],
+                room_graph[i][0][1]
+                )
         self.room_grid = []
         grid_size += 1
         self.grid_size = grid_size
@@ -25,13 +35,21 @@ class World:
             room = self.rooms[room_id]
             self.room_grid[room.x][room.y] = room
             if 'n' in room_graph[room_id][1]:
-                self.rooms[room_id].connect_rooms('n', self.rooms[room_graph[room_id][1]['n']])
+                self.rooms[room_id].connect_rooms(
+                    'n', self.rooms[room_graph[room_id][1]['n']]
+                    )
             if 's' in room_graph[room_id][1]:
-                self.rooms[room_id].connect_rooms('s', self.rooms[room_graph[room_id][1]['s']])
+                self.rooms[room_id].connect_rooms(
+                    's', self.rooms[room_graph[room_id][1]['s']]
+                    )
             if 'e' in room_graph[room_id][1]:
-                self.rooms[room_id].connect_rooms('e', self.rooms[room_graph[room_id][1]['e']])
+                self.rooms[room_id].connect_rooms(
+                    'e', self.rooms[room_graph[room_id][1]['e']]
+                    )
             if 'w' in room_graph[room_id][1]:
-                self.rooms[room_id].connect_rooms('w', self.rooms[room_graph[room_id][1]['w']])
+                self.rooms[room_id].connect_rooms(
+                    'w', self.rooms[room_graph[room_id][1]['w']]
+                    )
         self.starting_room = self.rooms[0]
 
     def print_rooms(self, visited=set(), backtrack=[]):
@@ -40,9 +58,11 @@ class World:
             rotated_room_grid.append([None] * len(self.room_grid))
         for i in range(len(self.room_grid)):
             for j in range(len(self.room_grid[0])):
-                rotated_room_grid[len(self.room_grid[0]) - j - 1][i] = self.room_grid[i][j]
-        print("#####")
-        str = ""
+                rotated_room_grid[
+                    len(self.room_grid[0]) - j - 1
+                    ][i] = self.room_grid[i][j]
+        print('#####')
+        str = ''
         for row in rotated_room_grid:
             all_null = True
             for room in row:
@@ -52,42 +72,42 @@ class World:
             if all_null:
                 continue
             # PRINT NORTH CONNECTION ROW
-            str += "#"
+            str += '#'
             for room in row:
                 if room is not None and room.n_to is not None:
-                    str += "  |  "
+                    str += '  |  '
                 else:
-                    str += "     "
-            str += "#\n"
+                    str += '     '
+            str += '#\n'
             # PRINT ROOM ROW
-            str += "#"
+            str += '#'
             for room in row:
                 if room is not None and room.w_to is not None:
-                    str += "-"
+                    str += '-'
                 else:
-                    str += " "
+                    str += ' '
                 if room is not None:
                     if room.id in backtrack:
-                        str += f"▒▒▒".zfill(3)
+                        str += f'▒▒▒'.zfill(3)
                     else:
                         if room.id not in visited:
-                            str += f"{room.id}".zfill(3)
+                            str += f'{room.id}'.zfill(3)
                         else:
-                            str += f"███".zfill(3)
+                            str += f'███'.zfill(3)
                 else:
-                    str += "   "
+                    str += '   '
                 if room is not None and room.e_to is not None:
-                    str += "-"
+                    str += '-'
                 else:
-                    str += " "
-            str += "#\n"
+                    str += ' '
+            str += '#\n'
             # PRINT SOUTH CONNECTION ROW
-            str += "#"
+            str += '#'
             for room in row:
                 if room is not None and room.s_to is not None:
-                    str += "  |  "
+                    str += '  |  '
                 else:
-                    str += "     "
-            str += "#\n"
+                    str += '     '
+            str += '#\n'
         print(str)
-        print("#####")
+        print('#####')
